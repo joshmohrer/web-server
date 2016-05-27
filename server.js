cf2df539616b62497f06efbcb34f2abd3d73267e
Josh Mohrer <joshmohrer@gmail.com>
2016-05-27 12:03:45 -0400

    intialize repo

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var PORT = 3000
+
+var middleWare = {
+	requireAuthentication: function (req, res, next){
+		console.log('private route hit');
+		next()
+	},
+	logger: function(req, res, next){
+		console.log(new Date().toString() + 'Request: ' + req.method + ' ' + req.originalUrl);
+		next();
+	}
+}
+
+app.use(middleWare.logger);
+
+// app.get('/', function(req, res){
+// 	res.send('Hello Express!');
+// });
+
+app.get('/about', middleWare.requireAuthentication, function(req, res){
+	res.send('Hello About!');
+})
+
+app.use(express.static(__dirname + '/public'));
+// console.log(__dirname);
+
+app.listen(PORT, function(){
+	console.log('Express server started on port ' + PORT + '!')
+});
